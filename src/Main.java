4cc14cefe2c86b25a65405925ffb0797036bdbdf
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-03-19 23:54:55 +0000

    feat main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
- class Main {
+public class Main {
+    public static void main(String[] args) {
+        Guerreiro guerreiro = new Guerreiro("Arthur", 100, 20, "Espada Longa");
+        Mago mago = new Mago("Merlin", 80, 10, 100);
 
-    public static void main(String[] args){
-
-        
-
+        guerreiro.atacar();
+        mago.atacar();
     }
-
 }
