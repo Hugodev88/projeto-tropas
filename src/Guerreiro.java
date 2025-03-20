416c2bbc2ce31b76fe26cc57ca5bf2ad6b57ac5a
JVjoao07 <102702359+JVjoao07@users.noreply.github.com>
2025-03-19 21:05:47 -0300

    Update Guerreiro.java

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -1,20 +1,12 @@
 // Subclasse Guerreiro
 public class Guerreiro extends Personagem {
-    private int defesa;
-    private String arma;
+    private String espada;
 
-    public Guerreiro(String nome, int vida, int nivel, int dano, int defesa, String arma) {
-        super(nome, vida, nivel, dano);
-        this.defesa = defesa;
-        this.arma = arma;
+    public Guerreiro(String nome, int vida, int forca, String espada) {
+        super(nome, vida, forca);
+        this.espada = espada;
     }
 
-    @Override
-    public void atacar() {
-        System.out.println(nome + " ataca com " + arma + " causando " + dano + " de dano!");
-    }
 
-    public void defender() {
-        System.out.println(nome + " se defende e reduz " + defesa + " de dano!");
     }
 }
