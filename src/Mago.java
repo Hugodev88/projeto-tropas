97986aee5b60ce4f0eb6443fd0a522445e630532
JVjoao07 <102702359+JVjoao07@users.noreply.github.com>
2025-03-19 21:08:32 -0300

    Update Mago.java

--- src/Mago.java
+++ src/Mago.java
@@ -1,21 +1,13 @@
 // Subclasse Mago
 public class Mago extends Personagem {
     private int mana;
-    private String magia;
 
-    public Mago(String nome, int vida, int nivel, int dano, int mana, String magia) {
-        super(nome, vida, nivel, dano);
+
+    public Mago(String nome, int vida , int forca ,int mana) {
+        super(nome, vida, forca);
         this.mana = mana;
-        this.magia = magia;
+        
     }
 
-    @Override
-    public void atacar() {
-        System.out.println(nome + " lança " + magia + " causando " + dano + " de dano mágico!");
-    }
 
-    public void recuperarMana() {
-        mana += 10;
-        System.out.println(nome + " recuperou mana. Mana atual: " + mana);
-    }
 }
